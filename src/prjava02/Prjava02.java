07d3a3bace18b066a74ee06b2070674dd73e290f
jopamu1314daw2 <jopamu1314daw2@gmail.com>
2014-04-23 13:26:36 +0200

    Treballant amb ordres git i brancas v0.1

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -14,6 +14,7 @@ InetAddress addr = InetAddress.getLocalHost(); byte[] ipAddr = addr.getAddress()
 String hostname = addr.getHostName(); System.out.println("hostname="+hostname);
 System.out.println("Nom de l'usuari: " + System.getProperty("user.name")); System.out.println("Carpeta Personal: " + System.getProperty("user.home")); System.out.println("Sistema operatiu: " + System.getProperty("os.name")); System.out.println("Versió OS: " + System.getProperty("os.version"));
 System.out.println("Arquitecura OS: " + System.getProperty("os.arch"));
+System.out.println("Creació d'una branca del projecte prjava02");
 }
 catch (IOException e) { e.printStackTrace();
 }}}
